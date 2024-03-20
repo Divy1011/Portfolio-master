7af0c6c30bcf998dcaf57e6b688231c03ad1689a
Divy Patel <divypatidar101@gmail.com>
2024-03-20 14:32:31 +0530

    Initial commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Divy Patel </span>
+            from <span className="purple"> Ahmedabad, India.</span>
+            <br />
+            I am currently ReactJs Developer Intern at iCreatewebtech.
+            <br />
+            I have completed BTech in Information Technology at Silver Oak 
+            University.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Movies
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Learning is a treasure that is following me everywhere."{" "}
+          </p>
+          <footer className="blockquote-footer">Divy</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import editor from "../../Assets/Projects/todo.png";
+import chatify from "../../Assets/Projects/text.png";
+import bitsOfCode from "../../Assets/Projects/news.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked for Learning purpose.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="TextUtills"
+              description="Web-based text editor used for text manipulation tasks such as converting text to upper and lower case, as well as measuring the length of text."
+              ghLink="https://github.com/Divy1011/TextUtills"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="NewsMonkey"
+              description="This is a demo project for a news app created using the News API. It features various types of news, including sports, business, entertainment, general, and health. This project serves as a part of my learning process with APIs."
+              ghLink="https://github.com/Divy1011/newsapp"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="ToDo App"
+              description="This is a Todo list demo project created with Node.js as the backend, utilizing Express APIs. CRUD operations are performed using a MongoDB database. This project serves as a learning experience.t"
+              ghLink="https://github.com/Divy1011/todo-website-demo"             
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
