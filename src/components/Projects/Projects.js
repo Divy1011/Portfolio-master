b86ef5490a42dcc4fc3274652ae56c9a391c14d2
Divy Patel <divypatidar101@gmail.com>
2024-05-01 15:45:42 +0530

    final edit project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import editor from "../../Assets/Projects/todo.png";
-import chatify from "../../Assets/Projects/text.png";
 import bitsOfCode from "../../Assets/Projects/news.png";
 import electric from "../../Assets/Projects/ss.png"
 import portfolio from "../../Assets/Projects/ss2.png"
 import ss3 from "../../Assets/Projects/ss3.png"
 import ss4 from "../../Assets/Projects/ss4.png"
+import ss5 from "../../Assets/Projects/ss5.png"
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -59,12 +58,12 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={ss5}
               isBlog={false}
               title="News Monkey"
-              description="A demo project for a news app created using the News API. It features various types of news, including sports, business, entertainment, general, and health. This project serves as a part of my learning process with APIs."
+              description="A demo project for a news app created using the free News API. It features various types of news, including sports, business, entertainment, general, and health. This project serves as a part of my learning process with APIs."
               ghLink="https://github.com/Divy1011/newsapp"
-              demoLink="https://divy-demosite.netlify.app/"
+              demoLink="https://newsmonkey-apidemo.netlify.app/"
             />
           </Col>
 
